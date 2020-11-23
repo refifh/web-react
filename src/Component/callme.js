64eef02489ee83629d973309ac9e9228b62267bf
refifahriza <refi.refifh@gmail.com>
2020-11-24 02:51:27 +0700

    make cards for detail contact

--- src/Component/callme.js
+++ src/Component/callme.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Card, CardDeck } from 'react-bootstrap';
 
 const Hubsaya = () => {
     return (
         <div>
             <Container id='letstalk'>
-                <h1>Lets Talk</h1>
+                <h1 Text style={{textAlignVertical: "center",textAlign: "center"}}>Do you have any Project? Let’s Talk</h1>
+                <CardDeck>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This card has supporting text below as a natural lead-in to additional content.{' '}
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
+                 </CardDeck>
             </Container>
         </div>
     )
