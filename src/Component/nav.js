da1d45105a8916c9dc05f47bd8f17a07158efffb
refifahriza <refi.refifh@gmail.com>
2020-11-24 04:44:53 +0700

    make it fixed on the top

--- src/Component/nav.js
+++ src/Component/nav.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 
 const Navigasi = () => {
     return (
         <div>
-            <Nav bg="light" variant="light" className="justify-content-end">
-                <Nav.Item>
-                    <Nav.Link href="#profile">Profile</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link href="#pendidikan">Pendidikan</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link href="#pengalaman">Pengalaman</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link href="#letstalk">Hubungi Saya</Nav.Link>
-                </Nav.Item>
-                </Nav>
+            <Navbar fixed="top"  bg="light" variant="light" className="justify-content-end">
+                
+                    <Nav.Item>
+                        <Nav.Link href="#profil">Profil</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#pendidikan">Educations</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#pengalaman">Experience</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#letstalk">Hire Me</Nav.Link>
+                    </Nav.Item>
+                
+            </Navbar>
+            
         </div>
     );
 };
