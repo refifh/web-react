6d11371229f4e648ada5b9c54ffabed4c97c3ec5
refifahriza <refi.refifh@gmail.com>
2020-11-25 18:37:31 +0700

    Updates

---
+++ precache-manifest.5941c04d27c80b24901cfe14716c17b2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59dbac1b1f29cd6eacd0c9dddd01417f",
+    "url": "/web-react/index.html"
+  },
+  {
+    "revision": "4a765073c2afd73f8652",
+    "url": "/web-react/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e0f43529a3ed4fa88ae2",
+    "url": "/web-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4a765073c2afd73f8652",
+    "url": "/web-react/static/js/2.b4fe24fe.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/web-react/static/js/2.b4fe24fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0f43529a3ed4fa88ae2",
+    "url": "/web-react/static/js/main.5c3881b5.chunk.js"
+  },
+  {
+    "revision": "cacc9ec0de3e53b77f69",
+    "url": "/web-react/static/js/runtime-main.7578b04a.js"
+  },
+  {
+    "revision": "f452fc39e0dc47444daa23d9e71753b9",
+    "url": "/web-react/static/media/foto.f452fc39.png"
+  },
+  {
+    "revision": "7580f1a8026d3f803b3e41e267b0588e",
+    "url": "/web-react/static/media/instagram.7580f1a8.png"
+  },
+  {
+    "revision": "5ed19b7e4276413bff9bf72a39c713e9",
+    "url": "/web-react/static/media/latarblkg.5ed19b7e.jpg"
+  },
+  {
+    "revision": "e56013f89c9ecff666ab214ad6bf87b7",
+    "url": "/web-react/static/media/linkedin.e56013f8.png"
+  },
+  {
+    "revision": "6097ca9b23cc7c9c1e68bc5d01c0bc4b",
+    "url": "/web-react/static/media/logoemail.6097ca9b.png"
+  },
+  {
+    "revision": "e3df7b4c9119b483d35c8624ada0e41f",
+    "url": "/web-react/static/media/sman5.e3df7b4c.png"
+  },
+  {
+    "revision": "bcb8d2221ca5302b7473c57cca6ceb19",
+    "url": "/web-react/static/media/uii2.bcb8d222.jpg"
+  },
+  {
+    "revision": "30abb0d7e5d424b1d2aefa324f75254e",
+    "url": "/web-react/static/media/whatsapp.30abb0d7.png"
+  }
+]);
